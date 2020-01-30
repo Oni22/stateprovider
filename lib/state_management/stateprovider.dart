ed6e20f048dd6ced130c7198f09220ed21895383
Onur Özkan <onurozkan@Onurs-MBP.fritz.box>
2020-01-30 18:52:44 +0100

    scaffold added

--- lib/state_management/stateprovider.dart
+++ lib/state_management/stateprovider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:mobilepkg/state_management/statemodel.dart';
 
 class StateProviderListener extends StatefulWidget {
@@ -41,9 +42,8 @@ class _StateProviderListenerState extends State<StateProviderListener> {
           );
         }
         else {
-          return Container();
+          return Scaffold();
         }
-        
       },
     );
   }
