bf4ef497f04ac086abd48367fc1242d80de0421e
Onur Özkan <onurozkan@Onurs-MBP.fritz.box>
2020-01-30 18:19:05 +0100

    notify updated

--- lib/state_management/statemodel.dart
+++ lib/state_management/statemodel.dart
@@ -4,11 +4,11 @@ class StateModel<T> {
 
   final _stateController = StreamController<T>();
   Stream<T> get state => _stateController.stream;
-  StreamSink<T> get notify => _stateController.sink;
 
   void initState(T model) {
     _stateController.add(model);
   }
+  void notify(T model) => _stateController.sink.add(model);
 
   void dispose(){
     _stateController.close();
