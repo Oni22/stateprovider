6b6c5ab6d6685b99113c59bdd6981d40ad803452
Onur Özkan <onurozkan@Onurs-MBP.fritz.box>
2020-02-02 14:31:39 +0100

    stream store class changed

--- lib/src/statemodel.dart
+++ lib/src/statemodel.dart
@@ -6,7 +6,7 @@ class StreamStore<T> {
   Stream<T> get stream => _storeController.stream;
   StreamSink get _sink => _storeController.sink;
 
-  void notify(T model) => _sink.add(model);
+  void notify(T model) => _storeController.add(model);
 
   void dispose(){
     _storeController.close();
